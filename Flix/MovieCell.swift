cc206b06251f407da724e7973251a686dd838186
axpecial <and043@ucsd.edu>
2018-02-05 23:39:46 -0800

    Created a README

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Andy Duong on 2/4/18.
+//  Copyright © 2018 Andy Duong. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
