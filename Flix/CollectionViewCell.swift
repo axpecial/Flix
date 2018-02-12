2ba6ca2319908e8a33e92d586bce837ea36a949f
axpecial <and043@ucsd.edu>
2018-02-11 20:24:22 -0800

    Added a collection view and programmatically managed the layout

---
+++ Flix/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Flix
+//
+//  Created by Andy Duong on 2/11/18.
+//  Copyright © 2018 Andy Duong. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Flix/PosterCell.swift
@@ -0,0 +1,13 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Andy Duong on 2/11/18.
+//  Copyright © 2018 Andy Duong. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+}
